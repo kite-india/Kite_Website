d828e4865de69ca06c66170c438c8e4b853df5fb
daniel-jebarson <danieljebarson21@gmail.com>
2022-10-23 23:06:49 +0530

    Added responsiveness for sm 🛠️

--- sections/transaction/TransactCard.jsx
+++ sections/transaction/TransactCard.jsx
@@ -1,11 +1,11 @@
-import { Text, Box, Flex, Badge } from '@chakra-ui/react'
+import { Text, Box, Flex, Badge, Image } from '@chakra-ui/react'
 
 export default function TransactCard(props) {
   return (
     <Box
       py={'15px'}
       key={props.id}
-      maxWidth={{ md: '800px' }}
+      maxWidth={{ sm: '2000px', md: '800px' }}
       _hover={{ border: '3px solid #9ca75c' }}
       border={'3px solid #D9D9D9'}
       borderRadius="10px"
@@ -21,25 +21,27 @@ export default function TransactCard(props) {
         <Flex
           justifyContent={'space-between'}
           fontFamily={'Poppins'}
+          flexDirection={{ sm: 'column', md: 'row' }}
           pl={'25px'}
           py="15px"
         >
-          <img height={'70px'} width={'72px'} src={props.img} />
+          <Image height={'70px'} width={'72px'} src={props.img} />
+
           <Flex flexDirection={'column'} lineHeight={'25px'}>
             <Text
-              fontSize={{ sm: '13px', md: '17px', lg: '19px' }}
+              fontSize={{ sm: '16px', md: '17px', lg: '19px' }}
               fontWeight={'700'}
             >
               {props.place}
             </Text>
             <Text
-              fontSize={{ sm: '11px', md: '16px', lg: '18px' }}
+              fontSize={{ sm: '15px', md: '16px', lg: '18px' }}
               fontWeight={'500'}
             >
               {props.date}
             </Text>
             <Text
-              fontSize={{ sm: '11px', md: '17px', lg: '19px' }}
+              fontSize={{ sm: '16px', md: '17px', lg: '19px' }}
               fontWeight={'700'}
             >
               {props.guest} Guests
@@ -49,7 +51,7 @@ export default function TransactCard(props) {
             pr={{ sm: '10px', md: '40px', lg: '60px' }}
             mt={'15px'}
             fontWeight={'400'}
-            fontSize={{ sm: '16px', md: '28px', lg: '32px' }}
+            fontSize={{ sm: '24px', md: '28px', lg: '32px' }}
           >
             Rs : {props.cost}
           </Text>
@@ -57,7 +59,7 @@ export default function TransactCard(props) {
       </Box>
       <Text
         backgroundColor={'rgba(217, 217, 217, 0.22)'}
-        fontSize={{ sm: '12px', md: '14px', lg: '15px' }}
+        fontSize={{ sm: '14px', md: '14px', lg: '15px' }}
         fontWeight={'500px'}
         textAlign={'right'}
         textTransform={'upperCase'}
@@ -65,7 +67,7 @@ export default function TransactCard(props) {
         status :
         {props.status ? (
           <Badge
-            fontSize={{ sm: '12px', md: '14px', lg: '15px' }}
+            fontSize={{ sm: '14px', md: '14px', lg: '15px' }}
             mr={{ sm: '10px', md: '40px', lg: '60px' }}
             ml={'5px'}
             colorScheme={'white'}
