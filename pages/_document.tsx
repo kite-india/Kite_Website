9158fa63c00dd1fb7cdc9c69f451b1afe71c24cc
Suvigya Saxena <suvigya2001@gmail.com>
2022-10-26 22:03:06 +0530

    Fixing minor errors

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import type { DocumentContext } from 'next/document'
 
 export default class Document extends NextDocument {
-  static async getInitialProps(ctx: DocumentContext) {
-    return await Document.getInitialProps(ctx)
-  }
   render() {
     return (
       <Html lang="en">
