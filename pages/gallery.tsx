0e20d1eec1986f44bf95aa2ee6d33767d8c68b7a
satyasai002 <satyasaimadarapu@gmai.com>
2023-03-06 00:54:19 +0530

    added package page and gallery page

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import { Box, Text, Heading, Flex, Container, Grid } from '@chakra-ui/react'
+import {
+  Box,
+  Text,
+  Heading,
+  Flex,
+  Container,
+  Grid,
+  UnorderedList,
+  ListItem,
+  Image
+} from '@chakra-ui/react'
 import Layout from '@components/layouts/main'
-import Image from 'next/image'
 import Section from '@components/Section'
 import { NextPage } from 'next'
 import CustomImage from '@components/CustomImage'
+import GalleryCard from '@components/GalleryCard'
 
 const Gallery: NextPage = () => {
   return (
@@ -53,78 +63,7 @@ const Gallery: NextPage = () => {
             architecto libero in adipisci dolore officia corporis aperiam quia
             nulla. A, neque.
           </Text>
-          <Flex
-            direction="column"
-            my={5}
-            justifyContent="center"
-            maxW={{ xl: '1200px' }}
-          >
-            <Grid
-              w="full"
-              gridTemplateColumns={{
-                sm: 'repeat( auto-fit, minmax(350px, 1fr) )',
-                md: 'repeat( auto-fit, minmax(400px, 1fr) )',
-                lg: 'repeat( auto-fit, minmax(350px, 1fr) )'
-              }}
-              gap={8}
-              px={3}
-            >
-              <CustomImage
-                src="package/kashmirpackage.jpg"
-                alt="card"
-                objectFit="cover"
-                layout="responsive"
-                sizes="100vw"
-                width={380}
-                height={240}
-              />
-              <CustomImage
-                src="package/lehladakh.webp"
-                alt="card"
-                objectFit="cover"
-                layout="responsive"
-                sizes="100vw"
-                width={380}
-                height={240}
-              />
-              <CustomImage
-                src="package/nepalpackage.jpg"
-                alt="card"
-                objectFit="cover"
-                layout="responsive"
-                sizes="100vw"
-                width={380}
-                height={240}
-              />
-              <CustomImage
-                src="package/rajasthanpackage.jpg"
-                alt="card"
-                objectFit="cover"
-                layout="responsive"
-                sizes="100vw"
-                width={380}
-                height={240}
-              />
-              <CustomImage
-                src="package/lehladakh.webp"
-                alt="card"
-                objectFit="cover"
-                layout="responsive"
-                sizes="100vw"
-                width={380}
-                height={240}
-              />
-              <CustomImage
-                src="package/nepalpackage.jpg"
-                alt="card"
-                objectFit="cover"
-                layout="responsive"
-                sizes="100vw"
-                width={380}
-                height={240}
-              />
-            </Grid>
-          </Flex>
+
           <hr
             style={{
               width: '100%',
@@ -140,6 +79,76 @@ const Gallery: NextPage = () => {
           />
           <hr style={{ backgroundColor: '#8FB339', height: '18px' }} />
           {/* Photos and Videos Section*/}
+          <Box mt={4}>
+            <Flex direction="row" w="100%" justifyContent="center">
+              <Flex direction="column">
+                <GalleryCard
+                  src={
+                    'https://drive.google.com/uc?export=view&id=1Rcq3M1AZQiYoUST2TxTGKgCn_zbkti99'
+                  }
+                />
+                <GalleryCard
+                  src={
+                    'https://drive.google.com/uc?export=view&id=1p9rh_eeFN1gNboPh3zrHMCDYPJRC8g81'
+                  }
+                />
+                <GalleryCard
+                  src={
+                    'https://drive.google.com/uc?export=view&id=19O3WXZVrraPMJaCbFdi5_Hrsc39f8c3W'
+                  }
+                />
+                <GalleryCard
+                  src={
+                    'https://drive.google.com/uc?export=view&id=15ljeIRN9IZ34OtBs_NRr1MFnNsGPwLhW'
+                  }
+                />
+                <GalleryCard
+                  src={
+                    'https://drive.google.com/uc?export=view&id=1nK9r98JgTsLBJwl7H-YmvV_er7MeNJy2'
+                  }
+                />
+              </Flex>
+              <Flex direction="column">
+                <GalleryCard
+                  src={
+                    'https://drive.google.com/uc?export=view&id=1tyfAg5WMfCz8KnpMfwYilLkM3OqqjZNM'
+                  }
+                />
+                <GalleryCard
+                  src={
+                    'https://drive.google.com/uc?export=view&id=1mHmpqX8iBla5ZnaL0PfIsGZs_9ctoLK5'
+                  }
+                />
+                <GalleryCard
+                  src={
+                    'https://drive.google.com/uc?export=view&id=1sMs4S5nmFxVUvRB4fUKOSScpJzbcwo58'
+                  }
+                />
+              </Flex>
+              <Flex direction="column" maxWidth="30%">
+                <GalleryCard
+                  src={
+                    'https://drive.google.com/uc?export=view&id=1yFR7p7JCJhXBnjwJ2RAo6OUVgXPGlhLr'
+                  }
+                />
+                <GalleryCard
+                  src={
+                    'https://drive.google.com/uc?export=view&id=1ukZI7x9W9QcdETPiYdwc_moZmIP1n9I3'
+                  }
+                />
+                <GalleryCard
+                  src={
+                    'https://drive.google.com/uc?export=view&id=1V6ksPOpWGH5aqF1qeYYWVncQjcZHyMIn'
+                  }
+                />
+                <GalleryCard
+                  src={
+                    'https://drive.google.com/uc?export=view&id=17wta27owsGcOzDPXFC073x2MqbxorAeW'
+                  }
+                />
+              </Flex>
+            </Flex>
+          </Box>
         </Section>
       </Container>
     </Layout>
--- pages/packages/[id]/index.tsx
+++ pages/packages/[id]/index.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
-import { Box, Container, Flex, Heading, Text } from '@chakra-ui/react'
-import Image from 'next/image'
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Text,
+  SimpleGrid,
+  Icon,
+  Divider,
+  Link,
+  Grid,
+  Image
+} from '@chakra-ui/react'
+import { FiMap } from 'react-icons/fi'
 import axios from 'axios'
 
 import { Section } from '@components/index'
@@ -9,6 +22,15 @@ import type { NextPage } from 'next'
 import type { Trip } from '@utils/types'
 import { useTripsStore } from '@utils/hooks/useTripsStore'
 import CustomImage from '@components/CustomImage'
+import { GrDocumentPdf } from 'react-icons/gr'
+import Packages from '@sections/trips/packages-section'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper'
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 
 interface PackagesPageProps {
   packages_data: Trip
@@ -16,33 +38,168 @@ interface PackagesPageProps {
 
 const Page: NextPage<PackagesPageProps> = ({ packages_data }) => {
   if (!packages_data) return null
+  const data = [0, 0, 0, 0]
   const { name, id, image } = packages_data
+  const activities = ['Skiing', 'River Rafting', 'Camp Fire', 'Music']
   return (
     <Layout title={name}>
       <Container w="100%" pt={8} maxWidth="container.xl">
         <Section delay={0.2}>
-          <Flex gap={6}>
-            <Box
-              borderRadius="lg"
-              w="640px"
-              h="360px"
-              overflow={'hidden'}
-              position="relative"
+          <Flex direction={{ sm: 'column', md: 'row' }} w="100%">
+            <Box w={{ sm: '100%', md: '50%' }}>
+              <CustomImage
+                src={image}
+                alt={id}
+                layout="responsive"
+                width={240}
+                height={200}
+              />
+            </Box>
+            <Flex
+              w={{ sm: '100%', md: '50%' }}
+              direction="column"
+              gap={4}
+              p={12}
+              ml={4}
+              fontSize={20}
+              fontFamily="'Poppins'"
             >
-              <CustomImage src={image} alt={id} layout="fill" priority />
-            </Box>
-            <Box p={6} maxWidth="50%">
-              <Heading fontWeight="semibold" fontSize="72px">
-                {name}
-              </Heading>
-              <Text fontFamily="'Poppins'" fontWeight="light" fontSize="22px">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-                distinctio quasi reiciendis libero, totam aut perspiciatis
-                debitis quod pariatur perferendis necessitatibus aliquam optio
-                porro eum aliquid assumenda. Maiores, nobis! Exercitationem!
+              <Heading fontSize={{ sm: '30', md: '64' }}>{name}</Heading>
+              <Flex direction="row" gap={4}>
+                <Text>5Days/ 4Nights</Text>
+                <Text>Kashmir</Text>
+              </Flex>
+              <Text>
+                Volutpat at sit curabitur duis tristique est. Pharetra vel, arcu
+                ultrices fringilla. Eu arcu dolor neque enim ac lectus
+                adipiscing proin. Neque, senectus tellus lectus molestie tortor
+                ut leo. Justo tellus a mattis nascetur condimentum purus orci
+                lobortis. Habitasse suscipit vivamus cras a tristique in.
               </Text>
-            </Box>
+              <Text>$ XX,XXX</Text>
+              <Flex direction="row" gap={4}>
+                <Button
+                  bg="#8FB339"
+                  _hover={{ bg: 'green.400' }}
+                  _focus={{ bg: 'green.400' }}
+                  color="white"
+                  fontFamily="'Roboto'"
+                  borderRadius="10px"
+                  px={{ base: 6, lg: 12 }}
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                >
+                  Book now
+                </Button>
+                <Button
+                  bg="#8FB339"
+                  _hover={{ bg: 'green.400' }}
+                  _focus={{ bg: 'green.400' }}
+                  color="white"
+                  fontFamily="'Roboto'"
+                  borderRadius="10px"
+                  px={{ base: 6, lg: 12 }}
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                >
+                  Contact us
+                </Button>
+              </Flex>
+            </Flex>
           </Flex>
+          <Box fontSize={20} mt={6}>
+            <Flex
+              direction={{ sm: 'column', md: 'row' }}
+              justify="space-evenly"
+              w="100%"
+            >
+              <Flex
+                direction="column"
+                borderRight={{ base: 0, md: '2px' }}
+                pr={6}
+                w="50%"
+              >
+                <Heading>Plan Includes:</Heading>
+                <SimpleGrid
+                  fontFamily="'Poppins'"
+                  height="150px"
+                  columns={2}
+                  alignItems={{ base: 'center', lg: 'left' }}
+                  w="100%"
+                  mb={4}
+                  gap={3}
+                  p={4}
+                >
+                  <Box w="100%">
+                    {activities.map(tag => (
+                      <Text key={tag} w="100%" p={1}>
+                        <Icon as={FiMap} mr={2} />
+                        {tag}
+                      </Text>
+                    ))}
+                  </Box>
+                  <Box w="100%">
+                    {activities.map(tag => (
+                      <Text key={tag} w="100%" p={1}>
+                        <Icon as={FiMap} mr={2} />
+                        {tag}
+                      </Text>
+                    ))}
+                  </Box>
+                </SimpleGrid>
+              </Flex>
+              <Flex direction="column" fontSize={20} gap={4} w="50%" px={8}>
+                <Heading>Extra Info:</Heading>
+                <Box border="2px" borderRadius="20px" borderColor="gray.200">
+                  <Flex p={8} direction="row" gap={6}>
+                    <GrDocumentPdf size={60} />
+                    <Flex direction="column" gap={2}>
+                      <Text>Camping Information</Text>
+                      <Link color="#8FB339" href="#">
+                        Download
+                      </Link>
+                    </Flex>
+                  </Flex>
+                </Box>
+              </Flex>
+            </Flex>
+          </Box>
+          <Box mt={8}>
+            <Heading>Gallery:</Heading>
+            <Swiper
+              modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+              breakpoints={{
+                768: {
+                  slidesPerView: 2
+                },
+                1024: {
+                  slidesPerView: 3
+                }
+              }}
+              spaceBetween={20}
+              autoplay={{
+                delay: 6000,
+                disableOnInteraction: false
+              }}
+              pagination={{ clickable: true }}
+              slidesPerView={1}
+            >
+              {data.map((key, i) => (
+                <SwiperSlide
+                  key={i}
+                  style={{ paddingBottom: '55px', height: '400px' }}
+                >
+                  <CustomImage
+                    src="static-public/Saly-44.svg"
+                    alt="image"
+                    layout="responsive"
+                    width={240}
+                    height={200}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </Box>
         </Section>
       </Container>
     </Layout>
