10b798370a15382bff16ea0f0db6e6e0a8d6a959
Suvigya Saxena <suvigya2001@gmail.com>
2023-06-16 23:25:41 +0530

    fix for S3_endpoint

--- components/CustomImage.tsx
+++ components/CustomImage.tsx
@@ -1,12 +1,12 @@
 import Image, { ImageProps } from 'next/image'
 import React from 'react'
+import { S3_URL } from '@utils/constants'
 
 const FALLBACK_IMAGE = process.env.NEXT_PUBLIC_FALLBACK_IMAGE
-const S3_ENDPOINT = process.env.NEXT_PUBLIC_S3_ENDPOINT
 
 const CustomImage = ({ src, ...rest }: ImageProps) => {
   if (src) {
-    src = S3_ENDPOINT + src
+    src = S3_URL + src
   }
 
   return <Image src={src} {...rest} alt={src as string} />
---
+++ utils/constants.ts
@@ -0,0 +1,3 @@
+export const S3_URL =
+  process.env.NEXT_PUBLIC_S3_ENDPOINT ||
+  'https://kite-frontend-storage-79e65883162600-staging.s3.ap-south-1.amazonaws.com/protected/'
