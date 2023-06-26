74d1831d55e56c7653b04f14373f39ec8b90daa4
Athulr32 <athulrithu32@gmail.com>
2023-06-26 13:18:32 +0530

    changed signin redirect

--- baseconfig.ts
+++ baseconfig.ts
@@ -27,8 +27,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000/",
-        "redirectSignOut": "http://localhost:3000/",
+        "redirectSignIn": "https://kitefy.in/",
+        "redirectSignOut": "https://kitefy.in/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS",
